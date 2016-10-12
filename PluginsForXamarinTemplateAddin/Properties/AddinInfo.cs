3120e87f8553c3f7170c3b37cdac6ed513a6164b
Crocus7724 <account@crocus7724.net>
2016-10-13 03:22:07 +0900

    初回コミット

---
+++ PluginsForXamarinTemplateAddin/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "PluginsForXamarinTemplateAddin",
+    Namespace = "PluginsForXamarinTemplateAddin",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("PluginsForXamarinTemplateAddin")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("PluginsForXamarinTemplateAddin")]
+[assembly: AddinAuthor("Yamamoto")]
